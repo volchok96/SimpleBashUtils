cec9b81a233761a71de2cfc6e733f0af44e013fd
Katerina Zakharova <kzakharova96@yandex.com>
2024-11-09 18:24:51 +0300

    Added tests for grep

--- src/grep/s21_grep.h
+++ src/grep/s21_grep.h
@@ -7,20 +7,19 @@
 #include <stdlib.h>
 #include <string.h>
 
-#define BUFFSIZE 4096 // size of data block on file systems (4-8 KB)
+#define BUFFSIZE 4096 // block size in file systems (4-8 KB)
 
 typedef struct {
   int e; // For regex
   int i; // Ignores case
-  int v; // Inverts result of searching
-  int c; // Number of lines where searching string is found
-  int l; // Number of files where searching string is found
-  int n; // Line number in the output of the searched string
-  int h; // Doesn't show files where searching string is found
-  int s; // Doesn't show messages about files that weren't found or couldn't be
-         // read
-  int f; // Gets regular expressions from file (needs arguments)
-  int o; // Shows only searched string without full line
+  int v; // Inverts the search result
+  int c; // Number of lines in which the search string is found
+  int l; // Number of files in which the search string is found
+  int n; // Line number in the output of the search string
+  int h; // Does not display files where the search string was found
+  int s; // Does not display messages about files that were not found or cannot be read
+  int f; // Gets regular expressions from a file (arguments are needed)
+  int o; // Displays only the matched string without the whole line
 
   long long int string_count;
   int match_lines_count;
@@ -32,12 +31,12 @@ typedef struct {
   char search_express[BUFFSIZE];
 } flags_grep;
 
-// void init_options(flags_greps * flag);
+// void init_options(flags_grep * flag);
 void check_flags_grep(int argc, char **argv, flags_grep *flag);
 void open_file(int argc, char **argv, flags_grep *flag);
 void flag_grep_e(flags_grep *flag);
-void flag_grep_f(char *argv[], flags_grep *flag);
-void search_and_print(char *argv[], FILE *file, flags_grep *flag,
+void flag_grep_f(char *argv[], flags_grep *flag); // changed const char *argv[] to char *argv[]
+void search_and_print(char *argv[], FILE *file, flags_grep *flag, // changed char *argv[] to const char *argv[]
                       int file_index);
 void is_it_files(int argc, char **argv, flags_grep *flag);
 extern int optind, opterr, optopt;
---
+++ src/grep/test_grep.sh
@@ -0,0 +1,92 @@
+#!/bin/bash
+
+COUNTER_SUCCESS=0
+COUNTER_FAIL=0
+DIFF_RES=""
+FILENAME1="src/grep/texts/1.txt"
+FILENAME2="src/grep/texts/2.txt"
+FILENAME3="src/grep/texts/3.txt"
+FILENAME4="src/grep/texts/4.txt"
+FILENAME5="src/grep/texts/5.txt"
+LOG_DIR="testtxt"
+
+# Make sure the directory for results exists
+mkdir -p $LOG_DIR
+
+# Patterns for testing
+SHABLO=""
+SHABLON="123"
+SHABLON2="abc"
+SHABLON3="[Xx]"
+SHABLON4="[A-Z]"
+SHABLON5="^[A-Z]"
+
+# Main testing loop
+for var in -v -c -l -n -i -h -s -o
+do
+    TEST1="$var $SHABLO \"$FILENAME1\" \"$FILENAME2\" \"$FILENAME3\" \"$FILENAME4\" \"$FILENAME5\""
+    ./s21_grep $TEST1 > "$LOG_DIR/s21_grep$var.txt"
+    grep $TEST1 > "$LOG_DIR/grep$var.txt"
+    DIFF_RES="$(diff -s "$LOG_DIR/s21_grep$var.txt" "$LOG_DIR/grep$var.txt")"
+    if [ "$DIFF_RES" == "Files $LOG_DIR/s21_grep$var.txt and $LOG_DIR/grep$var.txt are identical" ]
+    then
+        echo "SUCCESS $TEST1"
+        (( COUNTER_SUCCESS++ ))
+    else
+        echo "FAIL $TEST1"
+        echo "$TEST1" >> log.txt
+        (( COUNTER_FAIL++ ))
+    fi
+    rm "$LOG_DIR/s21_grep$var.txt" "$LOG_DIR/grep$var.txt"
+done
+
+# Check for multiple options in one test
+for var in -v -c -l -n -i -h -s -o
+do
+    for var2 in -v -c -l -n -i -h -s -o
+    do
+        if [ "$var" != "$var2" ]
+        then
+            TEST1="$var $var2 $SHABLO \"$FILENAME1\" \"$FILENAME2\" \"$FILENAME3\" \"$FILENAME4\" \"$FILENAME5\""
+            ./s21_grep $TEST1 > "$LOG_DIR/s21_grep${var}${var2}.txt"
+            grep $TEST1 > "$LOG_DIR/grep${var}${var2}.txt"
+            DIFF_RES="$(diff -s "$LOG_DIR/s21_grep${var}${var2}.txt" "$LOG_DIR/grep${var}${var2}.txt")"
+            if [ "$DIFF_RES" == "Files $LOG_DIR/s21_grep${var}${var2}.txt and $LOG_DIR/grep${var}${var2}.txt are identical" ]
+            then
+                echo "SUCCESS $TEST1"
+                (( COUNTER_SUCCESS++ ))
+            else
+                echo "FAIL $TEST1"
+                echo "$TEST1" >> log.txt
+                (( COUNTER_FAIL++ ))
+            fi
+            rm "$LOG_DIR/s21_grep${var}${var2}.txt" "$LOG_DIR/grep${var}${var2}.txt"
+        fi
+    done
+done
+
+# Testing with patterns
+for SHABLON_VAR in "$SHABLON" "$SHABLON2" "$SHABLON3" "$SHABLON4" "$SHABLON5"
+do
+    for var in -v -c -l -n -i -h -s -o
+    do
+        TEST1="$var \"$SHABLON_VAR\" \"$FILENAME1\" \"$FILENAME2\" \"$FILENAME3\" \"$FILENAME4\" \"$FILENAME5\""
+        ./s21_grep $TEST1 > "$LOG_DIR/s21_grep$var.txt"
+        grep $TEST1 > "$LOG_DIR/grep$var.txt"
+        DIFF_RES="$(diff -s "$LOG_DIR/s21_grep$var.txt" "$LOG_DIR/grep$var.txt")"
+        if [ "$DIFF_RES" == "Files $LOG_DIR/s21_grep$var.txt and $LOG_DIR/grep$var.txt are identical" ]
+        then
+            echo "SUCCESS $TEST1"
+            (( COUNTER_SUCCESS++ ))
+        else
+            echo "FAIL $TEST1"
+            echo "$TEST1" >> log.txt
+            (( COUNTER_FAIL++ ))
+        fi
+        rm "$LOG_DIR/s21_grep$var.txt" "$LOG_DIR/grep$var.txt"
+    done
+done
+
+# Final results
+echo "SUCCESS: $COUNTER_SUCCESS"
+echo "FAIL: $COUNTER_FAIL"
